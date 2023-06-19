f60de7b02093cd02c2c88137354b242b630d9400
Azam <azam2004@bk.ru>
2023-06-19 13:25:16 +0500

    First comit

---
+++ script.js
@@ -0,0 +1,95 @@
+
+//     const NumberOfFilms = +prompt("Сколько фильмов вы уже посмотрели");
+//     console.log(NumberOfFilms);
+
+//     const personalMovieDB = {
+// 	count: NumberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     private:false
+// };
+
+//     let seeMovie = prompt("Один из последних просмотренных фильмов?");
+//     let StarsMovie = prompt("На сколько оцените его?");
+    
+//   personalMovieDB.movies [seeMovie] = StarsMovie ;
+//   console.log(personalMovieDB);
+
+// console.log( NaN || 2 || undefined );
+
+
+// console.log( NaN && 2 && undefined );
+
+
+// console.log( 1 && 2 && 3 );
+
+
+// console.log( !1 && 2 || !3 );
+
+
+// console.log( 25 || null && !3 );
+
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log("Done!");
+// };
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log("Done!");
+// };
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log("Done!");
+// }
+
+
+
+// let result = "";
+// let length = 7;
+
+// for(i = 0; i < length; i++){
+//    console.log(`its I: ${i}`);
+//    for(j = 1; j < i; j++){
+//       console.log(`its J: ${j}`);
+//       result += "*";
+//    }
+   
+//    result += "\n";
+// }
+// console.log(result);
+const data = [5, 10, "Shopping", 20, "Homework"];
+    let A = 2;
+    let text = " - done";
+for(i = 0; i < data.length; i++){
+    if(data === Number){
+        A *= date;
+    }else{
+        text += data;
+    };
+}
+console.log(data);
